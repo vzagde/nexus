1291c65642858276f357f0c70a0b183c2bcbc4c4
Dawood Ahmed <dawoodahmed@Dipeshs-MacBook-Pro.local>
2017-09-13 19:16:49 +0530

    ipa with dummy data

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -63,7 +63,7 @@ $$(document).on('deviceready', function() {
     })
 	.done(function(res) {
     	load_ui = res;
-    	load_location_ui();
+    	// load_location_ui();
     })
     .fail(function(err) {
         myApp.hideIndicator();
@@ -82,161 +82,312 @@ $$(document).on('deviceready', function() {
   	// map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
 });
 
+// myApp.onPageInit('location', function(page) {
+// 	var location_status = page.query.loaction_load_status;
+// 	if (location_status == 'true') {
+// 		console.log("TRUE");
+// 		// $("#location_container").empty();
+// 		$("#location_container").html(load_ui.location_html);
+
+// 	    $.each(load_ui.mall_list, function(index, value) {
+// 	        var element = document.getElementById("map_container"+value.id);
+
+// 	        var mapTypeIds = [];
+// 	        for (var type in google.maps.MapTypeId) {
+// 	            mapTypeIds.push(google.maps.MapTypeId[type]);
+// 	        }
+
+// 	        mapTypeIds.push("OSM");
+// 	        mapTypeIds.push("MyGmap");
+// 	        mapTypeIds.push("LocalGmap");
+// 	        mapTypeIds.push("WebStorageGmap");
+// 	        mapTypeIds.push("LocalMyGmap");
+// 	        mapTypeIds.push("WebStorageMyGmap");
+
+// 	        var map = new google.maps.Map(element, {
+// 	            center: new google.maps.LatLng(value.lat, value.long),
+// 	            zoom: 14,
+// 	            mapTypeId: "MyGmap",
+// 	            mapTypeControlOptions: {
+// 	                mapTypeIds: mapTypeIds,
+// 	                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+// 	            }
+// 	        });
+
+// 	        map.setOptions({
+// 	            draggable: false,
+// 	            zoomControl: false,
+// 	            scrollwheel: false,
+// 	            disableDoubleClickZoom: true
+// 	        });
+
+
+// 	        map.mapTypes.set("OSM", new google.maps.ImageMapType({
+// 	            getTileUrl: getOsmTileImgSrc,
+// 	            tileSize: new google.maps.Size(256, 256),
+// 	            name: "OSM",
+// 	            maxZoom: 15
+// 	        }));
+
+// 	        map.mapTypes.set("MyGmap", new google.maps.ImageMapType({
+// 	            getTileUrl: getGmapTileImgSrc,
+// 	            tileSize: new google.maps.Size(256, 256),
+// 	            name: "MyGmap",
+// 	            maxZoom: 15
+// 	        }));
+
+// 	        map.mapTypes.set("LocalGmap", new google.maps.ImageMapType({
+// 	            getTileUrl: getLocalTileImgSrc,
+// 	            tileSize: new google.maps.Size(256, 256),
+// 	            name: "LocalGmap",
+// 	            maxZoom: 15
+// 	        }));
+
+// 	        map.mapTypes.set("WebStorageGmap", new google.maps.ImageMapType({
+// 	            getTileUrl: getWebStorageTileImgSrc,
+// 	            tileSize: new google.maps.Size(256, 256),
+// 	            name: "WebStorageGmap",
+// 	            maxZoom: 15
+// 	        }));
+
+// 	        map.mapTypes.set("LocalMyGmap", new google.maps.ImageMapType({
+// 	            getTileUrl: function(coord, zoom) {
+// 	                return checkTileInSprites(coord, zoom) ?
+// 	                    getLocalTileImgSrc(coord, zoom) :
+// 	                    getGmapTileImgSrc(coord, zoom);
+// 	            },
+// 	            tileSize: new google.maps.Size(256, 256),
+// 	            name: "LocalMyGmap",
+// 	            maxZoom: 15
+// 	        }));
+
+// 	        map.mapTypes.set("WebStorageMyGmap", new google.maps.ImageMapType({
+// 	            getTileUrl: function(coord, zoom) {
+// 	                var image = getWebStorageTileImgSrc(coord, zoom);
+// 	                return image ? image : getGmapTileImgSrc(coord, zoom);
+// 	            },
+// 	            tileSize: new google.maps.Size(256, 256),
+// 	            name: "WebStorageMyGmap",
+// 	            maxZoom: 0
+// 	        }));
+
+// 	        google.maps.event.addListener(map, 'click', function(point) {
+// 	            var marker = new google.maps.Marker({
+// 	                position: point.latLng,
+// 	                map: map
+// 	            });
+
+// 	            google.maps.event.addListener(marker, 'dblclick', function() {
+// 	                marker.setMap(null);
+// 	            });
+
+// 	            google.maps.event.addListener(marker, 'click', function() {
+// 	                new google.maps.InfoWindow({
+// 	                    content: 'lat: ' + point.latLng.lat() + '<br>lng:' + point.latLng.lng()
+// 	                }).open(map, marker);
+// 	            });
+// 	        });
+
+// 	        function CustomControl(controlDiv, map, title, handler) {
+// 	            controlDiv.style.padding = '5px';
+
+// 	            var controlUI = document.createElement('DIV');
+// 	            controlUI.style.backgroundColor = 'white';
+// 	            controlUI.style.borderStyle = 'solid';
+// 	            controlUI.style.borderWidth = '2px';
+// 	            controlUI.style.cursor = 'pointer';
+// 	            controlUI.style.textAlign = 'center';
+// 	            controlUI.title = title;
+// 	            controlDiv.appendChild(controlUI);
+
+// 	            var controlText = document.createElement('DIV');
+// 	            controlText.style.fontFamily = 'Arial,sans-serif';
+// 	            controlText.style.fontSize = '12px';
+// 	            controlText.style.paddingLeft = '4px';
+// 	            controlText.style.paddingRight = '4px';
+// 	            controlText.innerHTML = title;
+// 	            controlUI.appendChild(controlText);
+
+// 	            google.maps.event.addDomListener(controlUI, 'click', handler);
+// 	        }
+
+// 	        var clearWebStorageDiv = document.createElement('DIV');
+// 	        var clearWebStorageButton = new CustomControl(clearWebStorageDiv, map,
+// 	            'Clear Web Storage', clearWebStorage);
+
+// 	        var prepareWebStorageDiv = document.createElement('DIV');
+// 	        var prepareWebStorageButton = new CustomControl(prepareWebStorageDiv, map,
+// 	            'Prepare Web Storage', prepareWebStorage);
+
+// 	        clearWebStorageDiv.index = 1;
+// 	        prepareWebStorageDiv.index = 1;
+// 	        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(clearWebStorageDiv);
+// 	        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(prepareWebStorageDiv);
+// 	    })
+
+// 		console.log("Data Appended");
+// 		setTimeout(function(){ 
+// 		    mainView.router.load({
+// 		        url: 'tabs.html',
+// 		        ignoreCache: false,
+// 		    });
+// 		}, 2000);
+// 	} else {
+// 		console.log("FALSE");
+// 		// $(".location_containers_hide").hide();
+// 		// $(".location_containers_"+mall_id).show();
+// 	}
+// })
 myApp.onPageInit('location', function(page) {
-	var location_status = page.query.loaction_load_status;
-	if (location_status == 'true') {
-		console.log("TRUE");
-		// $("#location_container").empty();
-		$("#location_container").html(load_ui.location_html);
+	$("#location_container").empty();
+	$("#location_container").html(load_ui.location_html);
 
-	    $.each(load_ui.mall_list, function(index, value) {
-	        var element = document.getElementById("map_container"+value.id);
+	$(".location_containers_hide").hide();
+	$(".location_containers_"+mall_id).show();
 
-	        var mapTypeIds = [];
-	        for (var type in google.maps.MapTypeId) {
-	            mapTypeIds.push(google.maps.MapTypeId[type]);
-	        }
+    $.each(load_ui.mall_list, function(index, value) {
+        var element = document.getElementById("map_container"+value.id);
 
-	        mapTypeIds.push("OSM");
-	        mapTypeIds.push("MyGmap");
-	        mapTypeIds.push("LocalGmap");
-	        mapTypeIds.push("WebStorageGmap");
-	        mapTypeIds.push("LocalMyGmap");
-	        mapTypeIds.push("WebStorageMyGmap");
+        var mapTypeIds = [];
+        for (var type in google.maps.MapTypeId) {
+            mapTypeIds.push(google.maps.MapTypeId[type]);
+        }
 
-	        var map = new google.maps.Map(element, {
-	            center: new google.maps.LatLng(value.lat, value.long),
-	            zoom: 14,
-	            mapTypeId: "MyGmap",
-	            mapTypeControlOptions: {
-	                mapTypeIds: mapTypeIds,
-	                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-	            }
-	        });
+        mapTypeIds.push("OSM");
+        mapTypeIds.push("MyGmap");
+        mapTypeIds.push("LocalGmap");
+        mapTypeIds.push("WebStorageGmap");
+        mapTypeIds.push("LocalMyGmap");
+        mapTypeIds.push("WebStorageMyGmap");
 
-	        map.setOptions({
-	            draggable: false,
-	            zoomControl: false,
-	            scrollwheel: false,
-	            disableDoubleClickZoom: true
-	        });
+        var map = new google.maps.Map(element, {
+            center: new google.maps.LatLng(value.lat, value.long),
+            zoom: 14,
+            mapTypeId: "MyGmap",
+            mapTypeControlOptions: {
+                mapTypeIds: mapTypeIds,
+                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+            }
+        });
+        var marker = new google.maps.Marker({
+			position: map.center,
+			label: "G",
+			map: map
+		});
+        map.setOptions({
+            draggable: false,
+            zoomControl: false,
+            scrollwheel: false,
+            disableDoubleClickZoom: true
+        });
 
 
-	        map.mapTypes.set("OSM", new google.maps.ImageMapType({
-	            getTileUrl: getOsmTileImgSrc,
-	            tileSize: new google.maps.Size(256, 256),
-	            name: "OSM",
-	            maxZoom: 15
-	        }));
+        map.mapTypes.set("OSM", new google.maps.ImageMapType({
+            getTileUrl: getOsmTileImgSrc,
+            tileSize: new google.maps.Size(256, 256),
+            name: "OSM",
+            maxZoom: 15
+        }));
 
-	        map.mapTypes.set("MyGmap", new google.maps.ImageMapType({
-	            getTileUrl: getGmapTileImgSrc,
-	            tileSize: new google.maps.Size(256, 256),
-	            name: "MyGmap",
-	            maxZoom: 15
-	        }));
+        map.mapTypes.set("MyGmap", new google.maps.ImageMapType({
+            getTileUrl: getGmapTileImgSrc,
+            tileSize: new google.maps.Size(256, 256),
+            name: "MyGmap",
+            maxZoom: 15
+        }));
 
-	        map.mapTypes.set("LocalGmap", new google.maps.ImageMapType({
-	            getTileUrl: getLocalTileImgSrc,
-	            tileSize: new google.maps.Size(256, 256),
-	            name: "LocalGmap",
-	            maxZoom: 15
-	        }));
+        map.mapTypes.set("LocalGmap", new google.maps.ImageMapType({
+            getTileUrl: getLocalTileImgSrc,
+            tileSize: new google.maps.Size(256, 256),
+            name: "LocalGmap",
+            maxZoom: 15
+        }));
 
-	        map.mapTypes.set("WebStorageGmap", new google.maps.ImageMapType({
-	            getTileUrl: getWebStorageTileImgSrc,
-	            tileSize: new google.maps.Size(256, 256),
-	            name: "WebStorageGmap",
-	            maxZoom: 15
-	        }));
+        map.mapTypes.set("WebStorageGmap", new google.maps.ImageMapType({
+            getTileUrl: getWebStorageTileImgSrc,
+            tileSize: new google.maps.Size(256, 256),
+            name: "WebStorageGmap",
+            maxZoom: 15
+        }));
 
-	        map.mapTypes.set("LocalMyGmap", new google.maps.ImageMapType({
-	            getTileUrl: function(coord, zoom) {
-	                return checkTileInSprites(coord, zoom) ?
-	                    getLocalTileImgSrc(coord, zoom) :
-	                    getGmapTileImgSrc(coord, zoom);
-	            },
-	            tileSize: new google.maps.Size(256, 256),
-	            name: "LocalMyGmap",
-	            maxZoom: 15
-	        }));
+        map.mapTypes.set("LocalMyGmap", new google.maps.ImageMapType({
+            getTileUrl: function(coord, zoom) {
+                return checkTileInSprites(coord, zoom) ?
+                    getLocalTileImgSrc(coord, zoom) :
+                    getGmapTileImgSrc(coord, zoom);
+            },
+            tileSize: new google.maps.Size(256, 256),
+            name: "LocalMyGmap",
+            maxZoom: 15
+        }));
 
-	        map.mapTypes.set("WebStorageMyGmap", new google.maps.ImageMapType({
-	            getTileUrl: function(coord, zoom) {
-	                var image = getWebStorageTileImgSrc(coord, zoom);
-	                return image ? image : getGmapTileImgSrc(coord, zoom);
-	            },
-	            tileSize: new google.maps.Size(256, 256),
-	            name: "WebStorageMyGmap",
-	            maxZoom: 0
-	        }));
+        map.mapTypes.set("WebStorageMyGmap", new google.maps.ImageMapType({
+            getTileUrl: function(coord, zoom) {
+                var image = getWebStorageTileImgSrc(coord, zoom);
+                return image ? image : getGmapTileImgSrc(coord, zoom);
+            },
+            tileSize: new google.maps.Size(256, 256),
+            name: "WebStorageMyGmap",
+            maxZoom: 0
+        }));
 
-	        google.maps.event.addListener(map, 'click', function(point) {
-	            var marker = new google.maps.Marker({
-	                position: point.latLng,
-	                map: map
-	            });
+        google.maps.event.addListener(map, 'click', function(point) {
+            var marker = new google.maps.Marker({
+                position: point.latLng,
+                map: map
+            });
 
-	            google.maps.event.addListener(marker, 'dblclick', function() {
-	                marker.setMap(null);
-	            });
+            google.maps.event.addListener(marker, 'dblclick', function() {
+                marker.setMap(null);
+            });
 
-	            google.maps.event.addListener(marker, 'click', function() {
-	                new google.maps.InfoWindow({
-	                    content: 'lat: ' + point.latLng.lat() + '<br>lng:' + point.latLng.lng()
-	                }).open(map, marker);
-	            });
-	        });
+            google.maps.event.addListener(marker, 'click', function() {
+                new google.maps.InfoWindow({
+                    content: 'lat: ' + point.latLng.lat() + '<br>lng:' + point.latLng.lng()
+                }).open(map, marker);
+            });
+        });
 
-	        function CustomControl(controlDiv, map, title, handler) {
-	            controlDiv.style.padding = '5px';
+        function CustomControl(controlDiv, map, title, handler) {
+            controlDiv.style.padding = '5px';
 
-	            var controlUI = document.createElement('DIV');
-	            controlUI.style.backgroundColor = 'white';
-	            controlUI.style.borderStyle = 'solid';
-	            controlUI.style.borderWidth = '2px';
-	            controlUI.style.cursor = 'pointer';
-	            controlUI.style.textAlign = 'center';
-	            controlUI.title = title;
-	            controlDiv.appendChild(controlUI);
+            var controlUI = document.createElement('DIV');
+            controlUI.style.backgroundColor = 'white';
+            controlUI.style.borderStyle = 'solid';
+            controlUI.style.borderWidth = '2px';
+            controlUI.style.cursor = 'pointer';
+            controlUI.style.textAlign = 'center';
+            controlUI.title = title;
+            controlDiv.appendChild(controlUI);
 
-	            var controlText = document.createElement('DIV');
-	            controlText.style.fontFamily = 'Arial,sans-serif';
-	            controlText.style.fontSize = '12px';
-	            controlText.style.paddingLeft = '4px';
-	            controlText.style.paddingRight = '4px';
-	            controlText.innerHTML = title;
-	            controlUI.appendChild(controlText);
+            var controlText = document.createElement('DIV');
+            controlText.style.fontFamily = 'Arial,sans-serif';
+            controlText.style.fontSize = '12px';
+            controlText.style.paddingLeft = '4px';
+            controlText.style.paddingRight = '4px';
+            controlText.innerHTML = title;
+            controlUI.appendChild(controlText);
 
-	            google.maps.event.addDomListener(controlUI, 'click', handler);
-	        }
+            google.maps.event.addDomListener(controlUI, 'click', handler);
+        }
 
-	        var clearWebStorageDiv = document.createElement('DIV');
-	        var clearWebStorageButton = new CustomControl(clearWebStorageDiv, map,
-	            'Clear Web Storage', clearWebStorage);
+        var clearWebStorageDiv = document.createElement('DIV');
+        var clearWebStorageButton = new CustomControl(clearWebStorageDiv, map,
+            'Clear Web Storage', clearWebStorage);
 
-	        var prepareWebStorageDiv = document.createElement('DIV');
-	        var prepareWebStorageButton = new CustomControl(prepareWebStorageDiv, map,
-	            'Prepare Web Storage', prepareWebStorage);
+        var prepareWebStorageDiv = document.createElement('DIV');
+        var prepareWebStorageButton = new CustomControl(prepareWebStorageDiv, map,
+            'Prepare Web Storage', prepareWebStorage);
 
-	        clearWebStorageDiv.index = 1;
-	        prepareWebStorageDiv.index = 1;
-	        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(clearWebStorageDiv);
-	        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(prepareWebStorageDiv);
-	    })
+        clearWebStorageDiv.index = 1;
+        prepareWebStorageDiv.index = 1;
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(clearWebStorageDiv);
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(prepareWebStorageDiv);
+    })
 
-		console.log("Data Appended");
-		setTimeout(function(){ 
-		    mainView.router.load({
-		        url: 'tabs.html',
-		        ignoreCache: false,
-		    });
-		}, 2000);
-	} else {
-		console.log("FALSE");
+		
 		// $(".location_containers_hide").hide();
 		// $(".location_containers_"+mall_id).show();
-	}
 })
 // Now we need to run the code that will be executed only for About page.
 
